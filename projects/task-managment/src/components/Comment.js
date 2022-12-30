6ac4e2f71a4264d7ec8af9224464e6066f771c46
noyon-podder <noyon.vai369@gmail.com>
2022-12-30 23:51:18 +0600

    message section implement

---
+++ projects/task-managment/src/components/Comment.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+const Comment = ({showModal, setShowModal}) => {
+    return (
+        <>
+        {showModal ? (
+          <>
+            <div
+              className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+            >
+              <div className="relative w-auto my-6 mx-auto max-w-3xl">
+                {/*content*/}
+                <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                  {/*header*/}
+                  <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
+                    <h3 className="text-3xl font-semibold">
+                      Comment
+                    </h3>
+                    <button
+                      className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                      onClick={() => setShowModal(false)}
+                    >
+                      <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                        ×
+                      </span>
+                    </button>
+                  </div>
+                  {/*body*/}
+                  <div className="relative p-6 flex-auto">
+  
+  
+                  <form >
+                  <label
+          for="title"
+          class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Enter Your Comment
+        </label>
+                 <input type="text" name='task' className='block border border-indigo-400 outline-[var(--main-color)] px-3 py-2 my-4 w-64 sm:w-80'/>
+                 <input type="submit" className='bg-[var(--main-color)] px-8 py-3 inline-block text-white rounded hover:bg-indigo-700' value="Submit" />
+  
+                 <button
+                      className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                      type="button"
+                      onClick={() => setShowModal(false)}
+                    >
+                      Close
+                    </button>
+                 </form>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+            <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+          </>
+        ) : null}
+  
+        
+      </>
+    );
+};
+
+export default Comment;
--- projects/task-managment/src/components/CompletedTask.js
+++ projects/task-managment/src/components/CompletedTask.js
@@ -1,70 +1,98 @@
-import React from 'react';
-import { toast } from 'react-hot-toast';
-import { Link, useLoaderData } from 'react-router-dom';
+import React, { useState } from "react";
+import { toast } from "react-hot-toast";
+import { Link, useLoaderData } from "react-router-dom";
+import Comment from "./Comment";
 
 const CompletedTask = () => {
-    const completedTaskData = useLoaderData();
+  const completedTaskData = useLoaderData();
+  const [showModal, setShowModal] = useState(null)
 
-    const completeTaskDelete = id => {
-        const confirm = window.confirm("Are you sure Delete this task")
-          if(confirm) {
-            fetch(`https://task-management-server-red.vercel.app/completedTaskDelete/${id}`, {
-                method: 'DELETE',
-            })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                if(data.deletedCount > 0){
-                    toast.success("Task was Deleted successfully")
-                }
-
-            })
+  const completeTaskDelete = (id) => {
+    const confirm = window.confirm("Are you sure Delete this task");
+    if (confirm) {
+      fetch(
+        `https://task-management-server-red.vercel.app/completedTaskDelete/${id}`,
+        {
+          method: "DELETE",
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          if (data.deletedCount > 0) {
+            toast.success("Task was Deleted successfully");
           }
+        });
     }
-    return (
-        <div>
-           
-<div className="overflow-x-auto relative">
-    <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+  };
+  return (
+    <div>
+      <div className="overflow-x-auto relative">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th scope="col" className="py-3 px-6">
-                    Completed Task name
-                </th>
-                <th scope="col" className="py-3 px-6">
-                    Delete
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  Button
-                </th>
+              <th scope="col" className="py-3 px-6">
+                Completed Task name
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Delete
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Button
+              </th>
+              <th scope="col" className="py-3 px-6">
+                Message
+              </th>
             </tr>
-        </thead>
-        <tbody>
-           {
-            completedTaskData.map(complete => <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={complete._id}>
-            <th scope="row" className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-               {complete.initialTask}
-            </th>
-            <td className="py-4 px-6">
-            <button onClick={() => completeTaskDelete(complete._id)}><i className="uil uil-trash-alt"></i> Delete</button>
-            </td>
-            <td className="py-4 px-6">
-            <Link to="/my-task"><button type="button" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-3 py-1.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Not Completed</button></Link>
-            </td>
-            <td className="py-4 px-6">
-               <button><i className="uil uil-message"> Comment</i></button>
-               
-            </td>
-        </tr>)
-             
-           }
-            
-        </tbody>
-    </table>
-</div>
-
-        </div>
-    );
+          </thead>
+          <tbody>
+            {completedTaskData.map((complete) => (
+              <tr
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                key={complete._id}
+              >
+                <th
+                  scope="row"
+                  className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                >
+                  {complete.initialTask}
+                </th>
+                <td className="py-4 px-6">
+                  <button onClick={() => completeTaskDelete(complete._id)}>
+                    <i className="uil uil-trash-alt"></i> Delete
+                  </button>
+                </td>
+                <td className="py-4 px-6">
+                  <Link to="/my-task">
+                    <button
+                      type="button"
+                      className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-3 py-1.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900"
+                    >
+                      Not Completed
+                    </button>
+                  </Link>
+                </td>
+                <td className="py-4 px-6">
+                <div onClick={() => setShowModal(true)}>
+               <button
+        className="hover:text-indigo-600 duration-500"
+        type="button"
+        
+    
+      >
+        <i className="uil uil-message mr-1"></i>
+        comment
+      </button>
+               </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <Comment showModal={showModal} setShowModal={setShowModal}/>
+    </div>
+  );
 };
 
 export default CompletedTask;
